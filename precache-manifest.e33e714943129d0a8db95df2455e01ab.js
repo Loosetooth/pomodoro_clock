d37d17967d565df80d4d7933733a1a1ff8454c21
Loosetooth <loosetooth@protonmail.com>
2020-08-20 18:29:44 +0200

    Updates

---
+++ precache-manifest.e33e714943129d0a8db95df2455e01ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385066bd368e57c920824422e10c10ec",
+    "url": "/pomodoro_clock/index.html"
+  },
+  {
+    "revision": "1aa0efa6c4d2be906c9e",
+    "url": "/pomodoro_clock/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9bcaa644ba71b93ea43f",
+    "url": "/pomodoro_clock/static/css/main.4f9151b5.chunk.css"
+  },
+  {
+    "revision": "1aa0efa6c4d2be906c9e",
+    "url": "/pomodoro_clock/static/js/2.ecbf5433.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/pomodoro_clock/static/js/2.ecbf5433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcaa644ba71b93ea43f",
+    "url": "/pomodoro_clock/static/js/main.455a230f.chunk.js"
+  },
+  {
+    "revision": "876221ed443e52accde0",
+    "url": "/pomodoro_clock/static/js/runtime-main.e6f5db23.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/pomodoro_clock/static/media/digital-7_mono.58045dab.ttf"
+  }
+]);
